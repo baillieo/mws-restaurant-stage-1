2120ae7f70ee1b9a4faf92a16c0686f731b2c545
Baillie O'Grady <baillieogrady@Baillies-MacBook-Air.local>
2018-07-22 00:50:20 +0100

    docs: Add more ARIA support

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -149,6 +149,7 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
  */
 createReviewHTML = (review) => {
   const li = document.createElement('li');
+  li.setAttribute('role', 'article');
   const name = document.createElement('p');
   name.innerHTML = review.name;
   li.appendChild(name);
